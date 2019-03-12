ee878f8841ce06c1168b797669af3f785cd7a2a4
Emilie <emmy.mar@hotmail>
2019-03-12 15:01:37 -0400

    Create JS skeleton document

---
+++ CV_revisited.js
@@ -0,0 +1,61 @@
+/*
+    List of functions for the BlocHacks2018 CV Generator project
+    Curated by: Émilie Martin, Daniela Venuta, Minh-Tam Do, Kevin Sidhu
+    Last modiied on: March 12th, 2019
+*/
+
+//contact information
+function addNum()
+{
+
+}
+function removeNum()
+{
+
+}
+
+//languages
+function addLang()
+{
+
+}
+function removeLang()
+{
+
+}
+
+//education
+function addSchool()
+{
+
+}
+function removeSchool()
+{
+
+}
+function addExtra()
+{
+
+}
+function removeExtra()
+{
+
+}
+
+//work experience
+function addWork()
+{
+
+}
+function removeWork()
+{
+
+}
+function addAccomp()
+{
+
+}
+function removedAccomp()
+{
+    
+}
